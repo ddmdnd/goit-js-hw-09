36865f556805a622905e1c40f6dc86bb244c2dcb
ddmdnd <139027634+ddmdnd@users.noreply.github.com>
2024-02-04 20:37:00 -0500

    ‘update’

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -2,68 +2,131 @@
 import "simplelightbox/dist/simple-lightbox.min.css";
     
 const images = [
+    // {
+    //   preview:
+    //   "../img/teamWork.png",
+    //   original:
+    //   "../img/teamWork4.png",
+    //   description: "Team Work",
+    // },
+    // {
+    //   preview:
+    //    "img/lamp.png",
+    //   original:
+    //   "img/lamp4.png",
+    //   description: "A lamp",
+    // },
+    // {
+    //   preview:
+    //   "/img/phone.png",
+    //   original:
+    //   "/img/phone4.png",
+    //   description: "A phone",
+    // },
+    // {
+    //   preview:
+    //   "/img/meetingRoom.png",
+    //   original:
+    //   "/img/meetingRoom4.png",
+    //   description: "A meeting room",
+    // },
+    // {
+    //   preview:
+    //   "/img/relaxZone.png",
+    //   original:
+    //   "/img/relaxZone4.png",
+    //   description: "Relax Zone",
+    // },
+    // {
+    //   preview:
+    //   "/img/office.png",
+    //   original:
+    //   "/img/office4.png",
+    //   description: "Office",
+    // },
+    // {
+    //   preview:
+    //     "/img/desk.png",
+    //   original:
+    //   "/img/desk4.png",
+    //   description: "A desk",
+    // },
+    // {
+    //   preview:
+    //   "/img/girlAndLaptom.png",
+    //   original:
+    //   "/img/girlAndLaptom4.png",
+    //   description: "A girl and a laptop",
+    // },
+    // {
+    //   preview:
+    //     "/img/meeting.png",
+    //   original:
+    //   "/img/meeting4.png",
+    //   description: "Meeting",
+    // },
     {
       preview:
-      "../img/teamWork.png",
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
       original:
-      "../img/teamWork4.png",
-      description: "Team Work",
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      description: "Hokkaido Flower",
     },
     {
       preview:
-       "img/lamp.png",
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
       original:
-      "img/lamp4.png",
-      description: "A lamp",
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+      description: "Container Haulage Freight",
     },
     {
       preview:
-      "/img/phone.png",
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
       original:
-      "/img/phone4.png",
-      description: "A phone",
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+      description: "Aerial Beach View",
     },
     {
       preview:
-      "/img/meetingRoom.png",
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
       original:
-      "/img/meetingRoom4.png",
-      description: "A meeting room",
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+      description: "Flower Blooms",
     },
     {
       preview:
-      "/img/relaxZone.png",
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
       original:
-      "/img/relaxZone4.png",
-      description: "Relax Zone",
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+      description: "Alpine Mountains",
     },
     {
       preview:
-      "/img/office.png",
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
       original:
-      "/img/office4.png",
-      description: "Office",
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+      description: "Mountain Lake Sailing",
     },
     {
       preview:
-        "/img/desk.png",
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
       original:
-      "/img/desk4.png",
-      description: "A desk",
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+      description: "Alpine Spring Meadows",
     },
     {
       preview:
-      "/img/girlAndLaptom.png",
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
       original:
-      "/img/girlAndLaptom4.png",
-      description: "A girl and a laptop",
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+      description: "Nature Landscape",
     },
     {
       preview:
-        "/img/meeting.png",
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
       original:
-      "/img/meeting4.png",
-      description: "Meeting",
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+      description: "Lighthouse Coast Sea",
     },
   ];
   // preventDefault();
