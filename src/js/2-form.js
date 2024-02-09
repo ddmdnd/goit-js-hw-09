e3573463c9e09edfe4756e4fba6c0a2578b936d8
ddmdnd <139027634+ddmdnd@users.noreply.github.com>
2024-02-08 22:24:15 -0500

    ‘update’

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,24 +1,80 @@
 const form = document.querySelector(".feedback-form")
 const textarea = form.querySelector("textarea")
-form.addEventListener("input", () => {
-    // preventDefault();
-    const localStringifyEmail = JSON.stringify(form.elements.email.value)
-    const localStringifyArea = JSON.stringify(form.elements.message.value)
-    localStorage.setItem("email", localStringifyEmail)
-    localStorage.setItem("Area", localStringifyArea)
-  
-   })
-if(form.elements.email.value === "" ) {
-    const localGet = localStorage.getItem("email")
-    const localGetParse = JSON.parse(localGet)
-    form.elements.email.value = localGetParse};
-if(form.elements.message.value === "" ) {
-        const localGetMessage = localStorage.getItem("Area")
-        const localGetParseMessage = JSON.parse(localGetMessage)
-        form.elements.message.value = localGetParseMessage};
+// const dataLocalStorage = JSON.parse(localStorage.getItem("feedback-form-state"))
+//     const dataEmail = dataLocalStorage.email;
+//     const dataMessage = dataLocalStorage.message;
+//     const emailValue = form.elements.email.value;
+//     const dataParseStorage = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+
+form.addEventListener("input", takeDataForm)
+function takeDataForm(ent){
+    ent.preventDefault();
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+    const dataForm = {
+        email,
+        message,
+    };
+    const localStringify = JSON.stringify(dataForm);
+    localStorage.setItem("feedback-form-state", localStringify);
+    
+}
+putDataForm();
+function putDataForm() {
+if(form.elements.email.value === "" || form.elements.email.value != JSON.parse(localStorage.getItem("feedback-form-state")).email){
+    form.elements.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+    };
+if(form.elements.message.value === "" || form.elements.message.value != JSON.parse(localStorage.getItem("feedback-form-state")).message){
+    form.elements.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    }
+};
+
+
+
+
+// takeDataLocal;
+
 form.addEventListener("submit", () => {
-    localStorage.removeItem("email")
-    localStorage.removeItem("Area")
-    form.reset()
-})
+    form.reset();
+    localStorage.removeItem("feedback-form-state");
+    
+} )
+
+// localStorage.removeItem("feedback-form-state");
+// function sendDataForm(){
+//     const email = form.elements.email.value;
+//     const message = form.elements.message.value;
+//     const dataForm = {
+//         email,
+//         message,
+//     };
+//     form.reset();
+//     localStorage.removeItem("feedback-form-state");
+    
+
+
+
+
+
+// form.addEventListener("input", () => {
+//     // preventDefault();
+//     const localStringifyEmail = JSON.stringify(form.elements.email.value)
+//     const localStringifyArea = JSON.stringify(form.elements.message.value)
+//     localStorage.setItem("email", localStringifyEmail)
+//     localStorage.setItem("Area", localStringifyArea)
+  
+//    })
+// if(form.elements.email.value === "" ) {
+//     const localGet = localStorage.getItem("email")
+//     const localGetParse = JSON.parse(localGet)
+//     form.elements.email.value = localGetParse};
+// if(form.elements.message.value === "" ) {
+//         const localGetMessage = localStorage.getItem("Area")
+//         const localGetParseMessage = JSON.parse(localGetMessage)
+//         form.elements.message.value = localGetParseMessage};
+// form.addEventListener("submit", () => {
+//     localStorage.removeItem("email")
+//     localStorage.removeItem("Area")
+//     form.reset()
+// })
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,26 @@
 import "./1-gallery";
 import "./2-form";
+// const form = document.querySelector(".feedback-form")
+// const textarea = form.querySelector("textarea")
+// form.addEventListener("input", () => {
+//     // preventDefault();
+//     const localStringifyEmail = JSON.stringify(form.elements.email.value)
+//     const localStringifyArea = JSON.stringify(form.elements.message.value)
+//     localStorage.setItem("email", localStringifyEmail)
+//     localStorage.setItem("Area", localStringifyArea)
+  
+//    })
+// if(form.elements.email.value === "" ) {
+//     const localGet = localStorage.getItem("email")
+//     const localGetParse = JSON.parse(localGet)
+//     form.elements.email.value = localGetParse};
+// if(form.elements.message.value === "" ) {
+//         const localGetMessage = localStorage.getItem("Area")
+//         const localGetParseMessage = JSON.parse(localGetMessage)
+//         form.elements.message.value = localGetParseMessage};
+// form.addEventListener("submit", () => {
+//     localStorage.removeItem("email")
+//     localStorage.removeItem("Area")
+//     form.reset()
+// })
+
